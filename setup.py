7887f56ec828630a98304d1a7cac8b02280fd219
MohammedFarzin <mohammedfarzin.m123@gmail.com>
2024-02-23 18:02:08 +0530

    custom exception created

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import find_packages, setup
+from typing import List
 
+def get_requirements(file_path) -> List[str]:
+    requirements = []
+    with  open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "")for req in requirements]
 
+        if '-e .' in requirements:
+            requirements = requirements.remove('-e .')
+
+    return requirements
 
 setup(
     name='mlproject',
@@ -8,5 +18,5 @@ setup(
     author='Farzin',
     author_email='mohammedfarzin.m123@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'seaborn']
+    install_requires=get_requirements('requirements.txt')
 )
